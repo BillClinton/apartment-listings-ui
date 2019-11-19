4c0027c11a73bef9bf7f0679932fa3b572ff9144
BillClinton <bclinton@appunto.net>
2019-11-19 18:51:26 -0500

    Correct creation action

--- src/reducers/ApartmentReducer.js
+++ src/reducers/ApartmentReducer.js
@@ -3,7 +3,10 @@ import { CREATE_APARTMENT, READ_APARTMENTS } from '../actions/types';
 export default (state = {}, action) => {
   switch (action.type) {
     case CREATE_APARTMENT:
-      return { ...state, apartments: [...state.apartments, action.payload] };
+      return {
+        ...state,
+        apartments: [...state.apartments, action.payload.apartment]
+      };
     case READ_APARTMENTS:
       return { ...state, apartments: action.payload };
 
