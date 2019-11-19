131122fe7ce1f4755d0cfd8af8565000793feb7b
BillClinton <bclinton@appunto.net>
2019-11-19 18:49:55 -0500

    Add bearer token for testing protected endpoints

--- src/apis/API.js
+++ src/apis/API.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDhhNzZlZWZlNDQyNjA4MzE3NGZjMzIiLCJpYXQiOjE1NzQyMDQ5NDd9.m1cBfXWGtBsHnxCbPQs5ns_ZkgP1NKY1WTNOM96DJrE';
+
 export default axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'http://localhost:3000',
+  headers: { Authorization: `Bearer ${token}` }
 });
