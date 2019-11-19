06b249bba712522d66cbd749b489907485d3f5c8
BillClinton <bclinton@appunto.net>
2019-11-19 18:54:18 -0500

    Add required fields

--- src/components/apartment/ApartmentForm.js
+++ src/components/apartment/ApartmentForm.js
@@ -23,8 +23,29 @@ const ApartmentForm = () => {
 
       <TextInput
         form={form}
+        fieldName="address"
+        label="Address"
+        validations={{ required: true }}
+      />
+
+      <TextInput
+        form={form}
         fieldName="rent"
         label="Rent"
+        validations={{ required: true }}
+      />
+
+      <TextInput
+        form={form}
+        fieldName="bedrooms"
+        label="Bedrooms"
+        validations={{ required: true }}
+      />
+
+      <TextInput
+        form={form}
+        fieldName="bathrooms"
+        label="Bathrooms"
         validations={{ required: true }}
       />
 
