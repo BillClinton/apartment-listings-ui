78ffc3fbda56d0dca5e0f949e5d30c45570b2f32
BillClinton <bclinton@appunto.net>
2019-11-29 17:33:32 -0500

    Add history to router, remove switch for now
    Also, move context to ApartmentHome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import { SemanticToastContainer } from 'react-semantic-toasts';
 import 'react-semantic-toasts/styles/react-semantic-alert.css';
 import AdminHeader from './components/layout/AdminHeader';
-import ApartmentStoreProvider from './contexts/ApartmentStore';
 import ApartmentHome from './components/admin/apartment/ApartmentHome';
+import history from './history';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <SemanticToastContainer position="top-left" />
-      <Router>
+      <Router history={history}>
         <AdminHeader />
-        <ApartmentStoreProvider>
-          <Switch>
-            <Route path="/admin/apartments">
-              <ApartmentHome />
-            </Route>
-          </Switch>
-        </ApartmentStoreProvider>
+        <Route path="/admin/apartments">
+          <ApartmentHome />
+        </Route>
       </Router>
     </div>
   );
