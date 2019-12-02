b59af2c48ee9ebf484b76ffaeb3166a4918afaa6
BillClinton <bclinton@appunto.net>
2019-12-01 22:17:31 -0500

    Move list item  styling to list component module

--- src/components/admin/apartment/ApartmentDetails.js
+++ src/components/admin/apartment/ApartmentDetails.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './styles/ApartmentDetails.module.scss';
 import { ReactComponent as EditIcon } from '../../../icons/edit-solid.svg';
 import { ReactComponent as DeleteIcon } from '../../../icons/minus-circle-solid.svg';
 
 const ApartmentDetails = ({ apartment }) => {
   return (
-    <li className={styles.details}>
-      <div className={styles.name}>{apartment.name}</div>
-      <div className={styles.rent}>{apartment.rent}</div>
-      <div className={styles.actions}>
+    <li>
+      <div>{apartment.name}</div>
+      <div>{apartment.rent}</div>
+      <div>
         <Link to={`/admin/apartments/edit/${apartment._id}`}>
-          <EditIcon className={styles.edit} />
+          <EditIcon className="edit" />
         </Link>
         <Link to={`/admin/apartments/delete/${apartment._id}`}>
-          <DeleteIcon className={styles.delete} />
+          <DeleteIcon className="delete" />
         </Link>
       </div>
     </li>
--- src/components/admin/apartment/ApartmentList.js
+++ src/components/admin/apartment/ApartmentList.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { ApartmentStore } from '../../../contexts/ApartmentStore';
 import ApartmentDetails from './ApartmentDetails';
+import NewButton from './NewButton';
 import ReloadButton from './ReloadButton';
+import styles from './styles/ApartmentList.module.scss';
 
 const ApartmentList = () => {
   const { store } = useContext(ApartmentStore);
@@ -9,8 +11,11 @@ const ApartmentList = () => {
 
   return apartments.length ? (
     <div className="apartment-list">
-      <h1>Apartment List</h1>
-      <ul>
+      <h1 className={styles.pageTitle}>
+        Apartment List
+        <NewButton />
+      </h1>
+      <ul className={styles.list}>
         {apartments.map(apartment => {
           return <ApartmentDetails apartment={apartment} key={apartment._id} />;
         })}
