7549694098e259e5f68cb58a39982b72753dd3b7
BillClinton <bclinton@appunto.net>
2019-12-02 02:06:26 -0500

    Update import paths

--- src/components/admin/apartment/ApartmentDetails.js
+++ src/components/admin/apartment/ApartmentDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ReactComponent as EditIcon } from '../../../icons/edit-solid.svg';
-import { ReactComponent as DeleteIcon } from '../../../icons/minus-circle-solid.svg';
+import { ReactComponent as EditIcon } from '../../icons/edit-solid.svg';
+import { ReactComponent as DeleteIcon } from '../../icons/minus-circle-solid.svg';
 
 const ApartmentDetails = ({ apartment }) => {
   return (
