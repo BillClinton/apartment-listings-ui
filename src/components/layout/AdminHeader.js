f9ab066512b45f58b5b24de84165002e3f94bae6
BillClinton <bclinton@appunto.net>
2019-12-07 17:13:24 -0500

    Import svg icons as ReactComponents

--- src/components/layout/AdminHeader.js
+++ src/components/layout/AdminHeader.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './AdminHeader.module.scss';
+import { ReactComponent as BarsIcon } from '../icons/bars-solid.svg';
+import { ReactComponent as CloseIcon } from '../icons/times-circle-solid.svg';
 
 const AdminHeader = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -27,33 +29,8 @@ const AdminHeader = () => {
           className={toggleClass.join(' ')}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
-          <svg
-            className={styles.menuOpen}
-            aria-hidden="true"
-            focusable="false"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path
-              fill="currentColor"
-              d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-            ></path>
-          </svg>
-
-          <svg
-            className={styles.menuClose}
-            aria-hidden="true"
-            focusable="false"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path
-              fill="currentColor"
-              d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"
-            ></path>
-          </svg>
+          <BarsIcon className={styles.menuOpen} />
+          <CloseIcon className={styles.menuClose} />
         </div>
       </div>
     </header>
