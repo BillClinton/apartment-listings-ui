bdfc4f2ea6d1e279d21b607b7c444ccb1626dae9
BillClinton <bclinton@appunto.net>
2019-12-07 02:08:07 -0500

    Add login form and protect admin route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import { SemanticToastContainer } from 'react-semantic-toasts';
 import 'react-semantic-toasts/styles/react-semantic-alert.css';
 import Admin from './components/admin/Admin';
 import history from './history';
 import './App.scss';
+import AuthContextProvider from './contexts/AuthContext';
+import LoginForm from './components/admin/auth/LoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <SemanticToastContainer position="top-left" />
-      <Router history={history}>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-      </Router>
-    </div>
+    <AuthContextProvider>
+      <div className="App">
+        <SemanticToastContainer position="top-left" />
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/">
+              <div>homepage goes here</div>
+            </Route>
+            <Route exact path="/login" component={LoginForm} />
+            <PrivateRoute path="/admin" component={Admin} />
+          </Switch>
+        </Router>
+      </div>
+    </AuthContextProvider>
   );
 }
 
