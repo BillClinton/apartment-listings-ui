07646494af108ba9d6788479b8d9322885312492
BillClinton <bclinton@appunto.net>
2019-12-07 01:58:17 -0500

    Remove static token from API

--- src/apis/API.js
+++ src/apis/API.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDhhNzZlZWZlNDQyNjA4MzE3NGZjMzIiLCJpYXQiOjE1NzQyMDQ5NDd9.m1cBfXWGtBsHnxCbPQs5ns_ZkgP1NKY1WTNOM96DJrE';
+// const token =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZDhhNzZlZWZlNDQyNjA4MzE3NGZjMzIiLCJpYXQiOjE1NzQyMDQ5NDd9.m1cBfXWGtBsHnxCbPQs5ns_ZkgP1NKY1WTNOM96DJrE';
+//   headers: { Authorization: `Bearer ${token}` }
 
 export default axios.create({
-  baseURL: 'http://localhost:3000',
-  headers: { Authorization: `Bearer ${token}` }
+  baseURL: 'http://localhost:3000'
 });
